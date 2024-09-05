4103a2974ccb9ab14c7cc1bb5683e11cd41b82b0
前田俊郎 <146901315+Maeda-Toshiro@users.noreply.github.com>
2024-09-05 16:16:18 +0900

    aa

---
+++ Novice/Playerh.h
@@ -0,0 +1,10 @@
+#pragma once
+#include"IntVector2.h"
+
+struct Player
+{
+	Vector2 pos;
+	Vector2 velo;
+	Vector2 acce;
+	int radius;
+};
--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,4 +1,5 @@
 #include <Novice.h>
+#include"Playerh.h"
 
 const char kWindowTitle[] = "学籍番号";
 
@@ -7,6 +8,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Player player{
+		{100,100},
+		{0,0,},
+		{5,1},
+		20,
+
+	};
+
+
+	/*int playerSize = 20;
+	int playerPosX = 100;
+	int playerPosY = 100;*/
+	
+	//bool bottomHit = false;
+	//bool sideHit = false;
+
+
+	
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -24,15 +44,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-	
-		///
+		
+		player.velo.x = 0;
+		//player.velo.y = 0;
+		
+		//if (keys[DIK_W]) {
+		player.acce.y /= 10;
+			player.velo.y += player.acce.y;
+			player.pos.y += player.velo.y;
+		//}
+
+		if (keys[DIK_S]) {
+			player.velo.y += player.acce.y;
+			player.pos.y += player.velo.y;
+		}
+
+		if (keys[DIK_A]) {
+			player.velo.x += player.acce.x;
+			player.pos.x -= player.velo.x;
+		}
+
+		if (keys[DIK_D]) {
+			player.velo.x += player.acce.x;
+			player.pos.x += player.velo.x;
+		}
 		/// ↑更新処理ここまで
+		///
 		///
 
 		///
 		/// ↓描画処理ここから
 		///
-
+		Novice::DrawBox((player.pos.x + 50), (player.pos.y + 50), player.radius, player.radius, 0, WHITE, kFillModeSolid);
 		///
 		/// ↑描画処理ここまで
 		///
