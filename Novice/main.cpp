4f6bb2df48abcb69dd12f6e61416930ac0d7cf1b
fujino-syouta <146901420+fujino-syouta@users.noreply.github.com>
2024-09-05 16:17:10 +0900

    a

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -6,12 +6,19 @@ const char kWindowTitle[] = "学籍番号";
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	Novice::Initialize(kWindowTitle, 720, 720);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	float PlayrPosX = 640;
+	float PlayrPosY = 0;
+
+	//落下速度
+	float speed = 1.0f;
+	float speed2 = 0.1f;
+	bool isflag = true;
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -24,15 +31,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-	
+		/// 
+		if (keys[DIK_A]) {
+			PlayrPosX -= 15;
+		}
+		
+		if (keys[DIK_D]) {
+			PlayrPosX += 15;
+		}
+		while (isflag==true)
+		{
+			PlayrPosY += speed;
+			speed += speed2;
+		}
+		
 		///
 		/// ↑更新処理ここまで
 		///
-
+	
 		///
 		/// ↓描画処理ここから
 		///
-
+		Novice::DrawEllipse((int)PlayrPosX, (int)PlayrPosY, 10, 10, 0.0f, WHITE,kFillModeSolid);
 		///
 		/// ↑描画処理ここまで
 		///
