10071e6217b03f6c22a6042a9f80c6c241782e77
fujino-syouta <146901420+fujino-syouta@users.noreply.github.com>
2024-09-11 11:49:22 +0900

    a

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -5,7 +5,7 @@ const char kWindowTitle[] = "学籍番号";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-
+	
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
